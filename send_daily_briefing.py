c11cdb635728d7ca86d3affd8725dac99e61fea1
hyunjae807-cell <hyunjae807@gmail.com>
2026-09-17 09:47:45 +0900

    Update send_daily_briefing.py

--- send_daily_briefing.py
+++ send_daily_briefing.py
@@ -15,7 +15,10 @@ GIST_ID = os.environ.get("GIST_ID", "")
 GIST_TOKEN = os.environ.get("GIST_TOKEN", "")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 KAKAO_REST_KEY = os.environ.get("KAKAO_REST_KEY", "") or "98117624d9baa3910b9d03fb295cc27c"
-KAKAO_AUTH_CODE = os.environ.get("KAKAO_AUTH_CODE", "") or "o0-SeFabVjKnUJIl5n0qqqzuFFQITqnDQPptX3BiqBqFP-xjrLzdowAAAAQKFyIgAAABoKy6bXVUdd9ffL_GXA"
+KAKAO_AUTH_CODE = (
+    os.environ.get("KAKAO_AUTH_CODE", "")
+    or "mitfc9wAAKfgQ8Hr7vsXy4x7sv3Raki62PsPvCmWsQf-KDJ16WIjFQAAAAQKFxItAAABoKzUOyZAPV-WDrAHcw"
+)
 
 TIMETREE_EMAIL = os.environ.get("TIMETREE_EMAIL", "")
 TIMETREE_PASSWORD = os.environ.get("TIMETREE_PASSWORD", "")
